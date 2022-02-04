bbc2706461da7b8de5cbf174c7db82b43fc9a4d1
EstrHuP <esthrhup@gmail.com>
2022-02-04 18:12:30 +0100

    phoneValidation - StringExtension

--- FreeGames-iOS/Utils/Extensions/StringExtensions.swift
+++ FreeGames-iOS/Utils/Extensions/StringExtensions.swift
@@ -22,9 +22,16 @@ extension String {
     }
 
     var isValidPhone: Bool {
-        let phoneNumberRegex = "^((\\+)|(00))[0-9]{6,14}$"
-        let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneNumberRegex)
-        let isValidPhone = phoneTest.evaluate(with: self)
-        return isValidPhone
+        do {
+            let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.phoneNumber.rawValue)
+            let matches = detector.matches(in: self, options: [], range: NSRange(location: 0, length: self.count))
+            if let res = matches.first {
+                return res.resultType == .phoneNumber && res.range.location == 0 && res.range.length == self.count
+            } else {
+                return false
+            }
+        } catch {
+            return false
+        }
     }
 }
